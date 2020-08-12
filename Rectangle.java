86662d7363141e9152f3a135df39580bf2f6fab6
DXCAlekhya <69515074+DXCAlekhya@users.noreply.github.com>
2020-08-11 21:42:08 -0700

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+public class rectangle
+{
+  double length,bradth;
+  public Rectangle(double length,double breadth)
+  {
+  this.length=length;
+  this.breadth=breadth;
+  }
+  public double area()
+  {
+  return length*breadth;
+  }
+ }
